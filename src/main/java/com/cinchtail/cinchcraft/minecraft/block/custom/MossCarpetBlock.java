068aab2a1aa910a16d9de1f59bc57fbd19181af4
cinchtail <cinchtail@gmail.com>
2022-10-20 15:58:50 -0400

    moss carpet

--- src/main/java/com/cinchtail/cinchcraft/minecraft/block/custom/MossCarpetBlock.java
+++ src/main/java/com/cinchtail/cinchcraft/minecraft/block/custom/MossCarpetBlock.java
@@ -4,15 +4,23 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.Property;
 
 public class MossCarpetBlock extends SnowLayerBlock {
+
+
+    private static final Property<?> LAYERS = BlockStateProperties.LAYERS;
+
     public MossCarpetBlock(BlockBehaviour.Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(LAYERS, 1));
+        this.registerDefaultState(this.stateDefinition.any());
     }
 
     @Override
@@ -28,5 +36,8 @@ public class MossCarpetBlock extends SnowLayerBlock {
     @Override
     public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos facingPos) {
         return !state.canSurvive(world, pos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, direction, neighborState, world, pos, facingPos);
+    }
+    public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_57096_) {
+        p_57096_.add(LAYERS);
     }
 }
