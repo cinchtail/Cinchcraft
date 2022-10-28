4df7a6300cd8ff1da4a1c537ff0bb4799dd18afa
cinchtail <cinchtail@gmail.com>
2022-10-28 15:19:14 -0400

    mixed forest tweaks

--- src/main/java/com/cinchtail/cinchcraft/world/village/VillageAddition.java
+++ src/main/java/com/cinchtail/cinchcraft/world/village/VillageAddition.java
@@ -34,30 +34,18 @@ public class VillageAddition {
                                           String nbtPieceRL,
                                           int weight) {
 
-        // Grabs the processor list we want to use along with our piece.
-        // This is a requirement as using the ProcessorLists.EMPTY field will cause the game to throw errors.
-        // The reason why is the empty processor list in the world's registry is not the same instance as in that field once the world is started up.
         Holder<StructureProcessorList> emptyProcessorList = processorListRegistry.getHolderOrThrow(EMPTY_PROCESSOR_LIST_KEY);
 
-        // Grab the pool we want to add to
         StructureTemplatePool pool = templatePoolRegistry.get(poolRL);
         if (pool == null) return;
 
-        // Grabs the nbt piece and creates a SinglePoolElement of it that we can add to a structure's pool.
-        // Use .legacy( for villages/outposts and .single( for everything else
         SinglePoolElement piece = SinglePoolElement.legacy(nbtPieceRL,
                 emptyProcessorList).apply(StructureTemplatePool.Projection.RIGID);
 
-        // Use AccessTransformer or Accessor Mixin to make StructureTemplatePool's templates field public for us to see.
-        // Weight is handled by how many times the entry appears in this list.
-        // We do not need to worry about immutability as this field is created using Lists.newArrayList(); which makes a mutable list.
         for (int i = 0; i < weight; i++) {
             pool.templates.add(piece);
         }
 
-        // Use AccessTransformer or Accessor Mixin to make StructureTemplatePool's rawTemplates field public for us to see.
-        // This list of pairs of pieces and weights is not used by vanilla by default but another mod may need it for efficiency.
-        // So lets add to this list for completeness. We need to make a copy of the array as it can be an immutable list.
         List<Pair<StructurePoolElement, Integer>> listOfPieceEntries = new ArrayList<>(pool.rawTemplates);
         listOfPieceEntries.add(new Pair<>(piece, weight));
         pool.rawTemplates = listOfPieceEntries;
@@ -77,11 +65,12 @@ public class VillageAddition {
                 "cinchcraft:plains_bee_keeper_house", 15);
 
         addBuildingToPool(templatePoolRegistry, processorListRegistry,
+                new ResourceLocation("minecraft:village/taiga/houses"),
+                "cinchcraft:taiga_bee_keeper_house", 15);
+
+        addBuildingToPool(templatePoolRegistry, processorListRegistry,
                 new ResourceLocation("minecraft:village/savanna/houses"),
                 "cinchcraft:savanna_weaponsmith_is_to_rare", 20);
 
-        addBuildingToPool(templatePoolRegistry, processorListRegistry,
-                new ResourceLocation("minecraft:village/taiga/houses"),
-                "cinchcraft:taiga_bee_keeper_house", 15);
     }
 }
