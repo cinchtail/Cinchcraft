2ba2337ba7c86c174e3a47eceefc020e8277c4e5
cinchtail <cinchtail@gmail.com>
2022-12-17 14:27:40 -0500

    mixins

---
+++ src/main/java/com/cinchtail/cinchcraft/mixin/MelonBlock.java
@@ -0,0 +1,13 @@
+package com.cinchtail.cinchcraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(Class.class)
+public class MelonBlock {
+
+    @Inject(at = @At("HEAD"), method = "method")
+    private void method(){
+    }
+}
