44953835419a9358f0502f5a9b182fe2146dff4a
cinchtail <cinchtail@gmail.com>
2022-12-19 18:52:55 -0500

    melon shearing event

--- src/main/java/com/cinchtail/cinchcraft/event/ModEvents.java
+++ src/main/java/com/cinchtail/cinchcraft/event/ModEvents.java
@@ -8,14 +8,14 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CarvedPumpkinBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -34,28 +34,27 @@ public class ModEvents {
             }
         }
         @SubscribeEvent
-        public static void MelonShearing(PlayerInteractEvent.RightClickBlock event, BlockHitResult blockHitResult, InteractionHand interactionHand) {
-            Player player = event.getEntity();
-            BlockPos pos = event.getPos();
-            Level level = player.level;
-            Block block = level.getBlockState(pos).getBlock();
+        public static InteractionResult MelonShearing(BlockState blockState, Level level, BlockPos pos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
             ItemStack itemstack = player.getItemInHand(interactionHand);
-            if(itemstack.canPerformAction(net.minecraftforge.common.ToolActions.SHEARS_CARVE)) {
-                if (block == Blocks.MELON) {
+            if (itemstack.canPerformAction(net.minecraftforge.common.ToolActions.SHEARS_CARVE)) {
+                if (!level.isClientSide) {
                     Direction direction = blockHitResult.getDirection();
                     Direction direction1 = direction.getAxis() == Direction.Axis.Y ? player.getDirection().getOpposite() : direction;
-                    level.playSound((Player)null, pos, SoundEvents.PUMPKIN_CARVE, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    level.playSound((Player) null, pos, SoundEvents.PUMPKIN_CARVE, SoundSource.BLOCKS, 1.0F, 1.0F);
                     level.setBlock(pos, ModBlocks.CARVED_MELON.get().defaultBlockState().setValue(CarvedPumpkinBlock.FACING, direction1), 11);
-                    ItemEntity itementity = new ItemEntity(level, (double)pos.getX() + 0.5D + (double)direction1.getStepX() * 0.65D, (double)pos.getY() + 0.1D, (double)pos.getZ() + 0.5D + (double)direction1.getStepZ() * 0.65D, new ItemStack(Items.MELON_SEEDS, 4));
-                    itementity.setDeltaMovement(0.05D * (double)direction1.getStepX() + level.random.nextDouble() * 0.02D, 0.05D, 0.05D * (double)direction1.getStepZ() + level.random.nextDouble() * 0.02D);
+                    ItemEntity itementity = new ItemEntity(level, (double) pos.getX() + 0.5D + (double) direction1.getStepX() * 0.65D, (double) pos.getY() + 0.1D, (double) pos.getZ() + 0.5D + (double) direction1.getStepZ() * 0.65D, new ItemStack(Items.MELON_SEEDS, 4));
+                    itementity.setDeltaMovement(0.05D * (double) direction1.getStepX() + level.random.nextDouble() * 0.02D, 0.05D, 0.05D * (double) direction1.getStepZ() + level.random.nextDouble() * 0.02D);
                     level.addFreshEntity(itementity);
-                    itemstack.hurtAndBreak(1, player, (broadcastBreakEvent) -> {
-                        broadcastBreakEvent.broadcastBreakEvent(interactionHand);
+                    itemstack.hurtAndBreak(1, player, (p_55287_) -> {
+                        p_55287_.broadcastBreakEvent(interactionHand);
                     });
                     level.gameEvent(player, GameEvent.SHEAR, pos);
                     player.awardStat(Stats.ITEM_USED.get(Items.SHEARS));
                 }
+
+                return InteractionResult.sidedSuccess(level.isClientSide);
             }
+            return null;
         }
     }
 }
