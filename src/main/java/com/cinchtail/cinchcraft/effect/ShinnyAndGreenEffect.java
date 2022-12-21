e0cd5d3054df70caf6f0f56481967370eba12707
cinchtail <cinchtail@gmail.com>
2022-12-21 14:12:43 -0500

    new potion effect

--- src/main/java/com/cinchtail/cinchcraft/effect/ShinnyAndGreenEffect.java
+++ src/main/java/com/cinchtail/cinchcraft/effect/ShinnyAndGreenEffect.java
@@ -1,22 +1,62 @@
 package com.cinchtail.cinchcraft.effect;
 
+import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.ai.gossip.GossipContainer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.item.trading.MerchantOffers;
 
 public class ShinnyAndGreenEffect extends MobEffect {
     protected ShinnyAndGreenEffect(MobEffectCategory mobEffectCategory, int i) {
         super(mobEffectCategory, i);
     }
-    @Override
-    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        if (!pLivingEntity.level.isClientSide()) {
+    private final GossipContainer gossips = new GossipContainer();
+    protected MerchantOffers offers;
 
+    protected void updateTrades() {
+
+    }
+
+    public MerchantOffers getOffers() {
+        if (this.offers == null) {
+            this.offers = new MerchantOffers();
+            this.updateTrades();
         }
-        super.applyEffectTick(pLivingEntity, pAmplifier);
+
+        return this.offers;
+    }
+
+    public void applyEffectTick(Player p_35541_) {
+        int i = this.getPlayerReputation(p_35541_);
+        if (i != 0) {
+            for(MerchantOffer merchantoffer : this.getOffers()) {
+                merchantoffer.addToSpecialPriceDiff(-Mth.floor((float)i * merchantoffer.getPriceMultiplier()));
+            }
+        }
+
+        if (p_35541_.hasEffect(MobEffects.HERO_OF_THE_VILLAGE)) {
+            MobEffectInstance mobeffectinstance = p_35541_.getEffect(MobEffects.HERO_OF_THE_VILLAGE);
+            int k = mobeffectinstance.getAmplifier();
+
+            for(MerchantOffer merchantoffer1 : this.getOffers()) {
+                double d0 = 0.3D + 0.0625D * (double)k;
+                int j = (int)Math.floor(d0 * (double)merchantoffer1.getBaseCostA().getCount());
+                merchantoffer1.addToSpecialPriceDiff(-Math.max(j, 1));
+            }
+        }
+
     }
     @Override
     public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
         return true;
+    }
+    public int getPlayerReputation(Player p_35533_) {
+        return this.gossips.getReputation(p_35533_.getUUID(), (p_186302_) -> {
+            return true;
+        });
     }
 }
