68276caa7fb4f24cba83a91e8f3d61ffcecd7e3e
cinchtail <cinchtail@gmail.com>
2022-12-21 12:43:43 -0500

    new potion effect

--- src/main/java/com/cinchtail/cinchcraft/effect/ModEffects.java
+++ src/main/java/com/cinchtail/cinchcraft/effect/ModEffects.java
@@ -2,14 +2,18 @@ package com.cinchtail.cinchcraft.effect;
 
 import com.cinchtail.cinchcraft.cinchcraft;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModEffects {
     public static final DeferredRegister<MobEffect> MOB_EFFECTS
     = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, cinchcraft.MOD_ID);
 
+    public static final RegistryObject<MobEffect> SHINNY_AND_GREEN = MOB_EFFECTS.register("shinny_and_green",
+            () -> new ShinnyAndGreenEffect(MobEffectCategory.BENEFICIAL, 3124687));
 
     public static void register(IEventBus eventBus) {
         MOB_EFFECTS.register(eventBus);
--- src/main/java/com/cinchtail/cinchcraft/effect/ShinnyAndGreenEffect.java
+++ src/main/java/com/cinchtail/cinchcraft/effect/ShinnyAndGreenEffect.java
@@ -2,9 +2,21 @@ package com.cinchtail.cinchcraft.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
 
 public class ShinnyAndGreenEffect extends MobEffect {
     protected ShinnyAndGreenEffect(MobEffectCategory mobEffectCategory, int i) {
         super(mobEffectCategory, i);
+    }
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+        if (!pLivingEntity.level.isClientSide()) {
+
+        }
+        super.applyEffectTick(pLivingEntity, pAmplifier);
+    }
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+        return true;
     }
 }
