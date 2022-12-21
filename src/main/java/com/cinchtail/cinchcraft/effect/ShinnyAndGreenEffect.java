8dbd9f053e12232e23a7d8d007aa6231e15e75a3
cinchtail <cinchtail@gmail.com>
2022-12-21 17:02:00 -0500

    new potion effect

--- src/main/java/com/cinchtail/cinchcraft/effect/ShinnyAndGreenEffect.java
+++ src/main/java/com/cinchtail/cinchcraft/effect/ShinnyAndGreenEffect.java
@@ -3,18 +3,17 @@ package com.cinchtail.cinchcraft.effect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.item.trading.MerchantOffers;
 
 public class ShinnyAndGreenEffect extends MobEffect {
-    protected ShinnyAndGreenEffect(MobEffectCategory mobEffectCategory, int i) {
-        super(mobEffectCategory, i);
+    public ShinnyAndGreenEffect(MobEffectCategory mobEffectCategory, int color) {
+        super(mobEffectCategory, color);
     }
     protected MerchantOffers offers;
-
     protected void updateTrades() {
-
     }
 
     public MerchantOffers getOffers() {
@@ -26,19 +25,18 @@ public class ShinnyAndGreenEffect extends MobEffect {
         return this.offers;
     }
 
-    public void applyEffectTick(Player b) {
-        if (b.hasEffect(ModEffects.SHINNY_AND_GREEN.get())) {
-            MobEffectInstance mobeffectinstance = b.getEffect(ModEffects.SHINNY_AND_GREEN.get());
-            assert mobeffectinstance != null;
-            int k = mobeffectinstance.getAmplifier();
-
-            for(MerchantOffer merchantoffer1 : this.getOffers()) {
-                double d0 = 0.3D + 0.0625D * (double)k;
-                int j = (int)Math.floor(d0 * (double)merchantoffer1.getBaseCostA().getCount());
-                merchantoffer1.addToSpecialPriceDiff(-Math.max(j, 1));
+    public void applyEffectTick(Player b, LivingEntity pLivingEntity, int pAmplifier) {
+        if (!pLivingEntity.level.isClientSide()) {
+                MobEffectInstance mobeffectinstance = b.getEffect(ModEffects.SHINNY_AND_GREEN.get());
+                assert mobeffectinstance != null;
+                int k = mobeffectinstance.getAmplifier();
+                for (MerchantOffer merchantoffer1 : this.getOffers()) {
+                    double d0 = 0.3D + 0.0625D * (double) k;
+                    int j = (int) Math.floor(d0 * (double) merchantoffer1.getBaseCostA().getCount());
+                    merchantoffer1.addToSpecialPriceDiff(-Math.max(j, 1));
+                }
+                super.applyEffectTick(pLivingEntity, pAmplifier);
             }
-        }
-
     }
     @Override
     public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
