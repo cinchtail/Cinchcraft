b03f6a4be33c2bbaa62ea123cd41c56e48b23360
cinchtail <cinchtail@gmail.com>
2023-01-22 19:10:51 -0500

    1.19.3 world gen stuff and sign fix

--- src/main/java/com/cinchtail/cinchcraft/block/entity/ModWoodTypes.java
+++ src/main/java/com/cinchtail/cinchcraft/block/entity/ModWoodTypes.java
@@ -3,5 +3,5 @@ package com.cinchtail.cinchcraft.block.entity;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class ModWoodTypes {
-    public static WoodType APPLE = WoodType.create("apple");
+    public static WoodType APPLE = WoodType.create("apple_sign");
 }
