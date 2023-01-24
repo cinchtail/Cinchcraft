c88bbf7046ed4f8b4b7c941d1b1b1312ea93a578
cinchtail <cinchtail@gmail.com>
2023-01-24 12:42:16 -0500

    Small cactus fixed

--- src/main/java/com/cinchtail/cinchcraft/block/custom/SmallCactusBlock.java
+++ src/main/java/com/cinchtail/cinchcraft/block/custom/SmallCactusBlock.java
@@ -7,13 +7,23 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.SuspiciousEffectHolder;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class SmallCactusBlock extends FlowerBlock {
-    public SmallCactusBlock(MobEffect mobEffect, int i, Properties properties) {
-        super(mobEffect, i, properties);
+public class SmallCactusBlock extends BushBlock {
+    protected static final float AABB_OFFSET = 3.0F;
+    protected static final VoxelShape SHAPE = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 10.0D, 11.0D);
+
+    public SmallCactusBlock(Properties properties) {
+        super(properties);
     }
+
 
     @Override
     protected boolean mayPlaceOn(BlockState blockState, BlockGetter blockGetter, BlockPos pos) {
@@ -22,5 +32,9 @@ public class SmallCactusBlock extends FlowerBlock {
     @Override
     public void entityInside(BlockState blockState, Level level, BlockPos pos, Entity entity) {
         entity.hurt(DamageSource.CACTUS, .5F);
+    }
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos pos, CollisionContext collisionContext) {
+        Vec3 vec3 = blockState.getOffset(blockGetter, pos);
+        return SHAPE.move(vec3.x, vec3.y, vec3.z);
     }
 }
